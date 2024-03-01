5020687cf68001b7102fb4f624ed769792e7309a
kwin <kwinthebest@gmail.com>
2024-03-01 10:11:04 +0700

    fix favorite

--- lib/feature/favorite/favorite_home_page.dart
+++ lib/feature/favorite/favorite_home_page.dart
@@ -1,37 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:qbooking/constant/key_storage_constant.dart';
-import 'package:qbooking/feature/favorite/widget/box_favorites.dart';
-import 'package:qbooking/model/room_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class FavoriteHomePage extends StatelessWidget {
   const FavoriteHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future<List<RoomModel>> getFavoriteRoom() async {
-      try {
-        SharedPreferences preferences = await SharedPreferences.getInstance();
-        final favoriteRoomsJson =
-            preferences.getString(KeyStorageConstant.favoriteRooms);
-        final List<RoomModel> favoriteRooms = favoriteRoomsJson != null
-            ? roomsModelFromJson(favoriteRoomsJson)
-            : [];
-        return favoriteRooms;
-      } catch (e) {
-        return [];
-      }
-    }
+    // Future<List<RoomModel>> getFavoriteRoom() async {
+    //   try {
+    //     SharedPreferences preferences = await SharedPreferences.getInstance();
+    //     final favoriteRoomsJson =
+    //         preferences.getString(KeyStorageConstant.favoriteRooms);
+    //     final List<RoomModel> favoriteRooms = favoriteRoomsJson != null
+    //         ? roomsModelFromJson(favoriteRoomsJson)
+    //         : [];
+    //     return favoriteRooms;
+    //   } catch (e) {
+    //     return [];
+    //   }
+    // }
    
-    Future<void> unfavorites(RoomModel roomData) async {
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      final favoriteRoomsJson =
-          preferences.getString(KeyStorageConstant.favoriteRooms);
-      final List<RoomModel> favoriteRooms = favoriteRoomsJson != null
-          ? roomsModelFromJson(favoriteRoomsJson)
-          : [];
-      favoriteRooms.where((element) => element.id != roomData.id); 
-    }
+    // Future<void> unfavorites(RoomModel roomData) async {
+    //   SharedPreferences preferences = await SharedPreferences.getInstance();
+    //   final favoriteRoomsJson =
+    //       preferences.getString(KeyStorageConstant.favoriteRooms);
+    //   final List<RoomModel> favoriteRooms = favoriteRoomsJson != null
+    //       ? roomsModelFromJson(favoriteRoomsJson)
+    //       : [];
+    //   favoriteRooms.where((element) => element.id != roomData.id); 
+    // }
 
     return Scaffold(
         appBar: AppBar(  
@@ -42,29 +39,29 @@ class FavoriteHomePage extends StatelessWidget {
           ),
           backgroundColor: Colors.white,
         ),
-        body: FutureBuilder<List<RoomModel>>(
-          future: getFavoriteRoom(),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return const Text('Error');
-              } else if (snapshot.hasData) {
-                return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (context, index) => BoxFavorites(
-                          roomData: snapshot.data[index],
-                        ));
-              } else {
-                return const Text('Empty data');
-              }
-            } else {
-              return Text('State: ${snapshot.connectionState}');
-            }
-          },
-        )
+        // body: FutureBuilder<List<RoomModel>>(
+        //   future: getFavoriteRoom(),
+        //   builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        //     if (snapshot.connectionState == ConnectionState.waiting) {
+        //       return const CircularProgressIndicator();
+        //     } else if (snapshot.connectionState == ConnectionState.done) {
+        //       if (snapshot.hasError) {
+        //         return const Text('Error');
+        //       } else if (snapshot.hasData) {
+        //         return ListView.builder(
+        //             shrinkWrap: true,
+        //             itemCount: snapshot.data.length,
+        //             itemBuilder: (context, index) => BoxFavorites(
+        //                   roomData: snapshot.data[index],
+        //                 ));
+        //       } else {
+        //         return const Text('Empty data');
+        //       }
+        //     } else {
+        //       return Text('State: ${snapshot.connectionState}');
+        //     }
+        //   },
+        // )
         );
   }
 }
--- lib/feature/favorite/widget/box_favorites.dart
+++ lib/feature/favorite/widget/box_favorites.dart
@@ -1,165 +1,166 @@
-import 'package:flutter/material.dart';
-import 'package:qbooking/constant/key_storage_constant.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qbooking/constant/key_storage_constant.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-import '../../../constant/image_constant.dart';
-import '../../../model/room_model.dart';
+// import '../../../constant/image_constant.dart';
+// import '../../../model/room_model.dart';
+// import '../../../model/room_model_one_model.dart';
 
-class BoxFavorites extends StatefulWidget {
-  const BoxFavorites({super.key, required this.roomData});
-    final RoomModel roomData;
+// class BoxFavorites extends StatefulWidget {
+//   const BoxFavorites({super.key, required this.roomData});
+//     final RoomModel roomData;
 
-  @override
-  State<BoxFavorites> createState() => _BoxFavoritesState();
-}
+//   @override
+//   State<BoxFavorites> createState() => _BoxFavoritesState();
+// }
 
-class _BoxFavoritesState extends State<BoxFavorites> {
-   Future<void> saveFavorite(RoomModel roomData) async {
-    try {
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      final favoriteRoomsJson =
-          preferences.getString(KeyStorageConstant.favoriteRooms);
-      final List<RoomModel> favoriteRooms = favoriteRoomsJson != null
-          ? roomsModelFromJson(favoriteRoomsJson)
-          : [];
-      final listSameRoom =
-          favoriteRooms.where((element) => element.id == roomData.id).toList();
-      if (listSameRoom.isNotEmpty) {
-      } else {
-        favoriteRooms.addAll([roomData]);
-      }
+// class _BoxFavoritesState extends State<BoxFavorites> {
+//   //  Future<void> saveFavorite(RoomModel roomData) async {
+//   //   try {
+//   //     SharedPreferences preferences = await SharedPreferences.getInstance();
+//   //     final favoriteRoomsJson =
+//   //         preferences.getString(KeyStorageConstant.favoriteRooms);
+//   //     final List<RoomModel> favoriteRooms = favoriteRoomsJson != null
+//   //         ? roomsModelFromJson(favoriteRoomsJson)
+//   //         : [];
+//   //     final listSameRoom =
+//   //         favoriteRooms.where((element) => element.id == roomData.id).toList();
+//   //     if (listSameRoom.isNotEmpty) {
+//   //     } else {
+//   //       favoriteRooms.addAll([roomData]);
+//   //     }
 
-      preferences.setString(
-          KeyStorageConstant.favoriteRooms, roomsModelToJson(favoriteRooms));
-      print(preferences.getString(KeyStorageConstant.favoriteRooms));
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return   Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: Card(
-            // shape: RoundedRectangleBorder(
-            //   borderRadius: BorderRadius.circular(15.0),
-            // ),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Image.asset(
-                        widget.roomData.images?[0] ?? ImageConstant.room1),
-                    Positioned(
-                      top: 8,
-                      right: 8,
-                      child: GestureDetector(
-                          onTap: () => saveFavorite(widget.roomData),
-                          child: const Icon(
-                           Icons.favorite
-                             ,
-                            color: Colors.red,
-                          )),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            'IQURI Room',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(
-                            width: 100,
-                          ),
-                          if (widget.roomData.isAvailable == true)
-                            const Row(
-                              children: [
-                                Icon(
-                                  Icons.circle,
-                                  color: Colors.green,
-                                  size: 10,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  'available now',
-                                  style: TextStyle(
-                                      fontSize: 13, color: Colors.green),
-                                ),
-                              ],
-                            )
-                          else
-                            const Row(
-                              children: [
-                                Icon(
-                                  Icons.circle,
-                                  color: Colors.red,
-                                  size: 10,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  'Unavailable now',
-                                  style: TextStyle(
-                                      fontSize: 13, color: Colors.red),
-                                ),
-                              ],
-                            )
-                        ],
-                      ),
-                      const Row(
-                        children: [
-                          Text(
-                            'Meeting room 204',
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Icon(
-                            Icons.circle,
-                            color: Colors.black,
-                            size: 10,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text('Floor 2')
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              widget.roomData.description ?? "",
-                              style: const TextStyle(fontSize: 13),
-                            ),
-                          ),
-                          const Icon(Icons.star_border_sharp),
-                          Text(widget.roomData.rate.toString())
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   //     preferences.setString(
+//   //         KeyStorageConstant.favoriteRooms, roomsModelToJson(favoriteRooms));
+//   //     print(preferences.getString(KeyStorageConstant.favoriteRooms));
+//   //   } catch (e) {
+//   //     print(e.toString());
+//   //   }
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return   Center(
+//       child: Padding(
+//         padding: const EdgeInsets.all(20.0),
+//         child: ClipRRect(
+//           borderRadius: BorderRadius.circular(30),
+//           child: Card(
+//             // shape: RoundedRectangleBorder(
+//             //   borderRadius: BorderRadius.circular(15.0),
+//             // ),
+//             child: Column(
+//               children: [
+//                 Stack(
+//                   children: [
+//                     Image.asset(
+//                         widget.roomData.images?[0] ?? ImageConstant.room1),
+//                     Positioned(
+//                       top: 8,
+//                       right: 8,
+//                       child: GestureDetector(
+//                           onTap: () => saveFavorite(widget.roomData),
+//                           child: const Icon(
+//                            Icons.favorite
+//                              ,
+//                             color: Colors.red,
+//                           )),
+//                     ),
+//                   ],
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(10.0),
+//                   child: Column(
+//                     children: [
+//                       Row(
+//                         children: [
+//                           const Text(
+//                             'IQURI Room',
+//                             style: TextStyle(
+//                                 fontSize: 20, fontWeight: FontWeight.bold),
+//                           ),
+//                           const SizedBox(
+//                             width: 100,
+//                           ),
+//                           if (widget.roomData.isAvailable == true)
+//                             const Row(
+//                               children: [
+//                                 Icon(
+//                                   Icons.circle,
+//                                   color: Colors.green,
+//                                   size: 10,
+//                                 ),
+//                                 SizedBox(
+//                                   width: 5,
+//                                 ),
+//                                 Text(
+//                                   'available now',
+//                                   style: TextStyle(
+//                                       fontSize: 13, color: Colors.green),
+//                                 ),
+//                               ],
+//                             )
+//                           else
+//                             const Row(
+//                               children: [
+//                                 Icon(
+//                                   Icons.circle,
+//                                   color: Colors.red,
+//                                   size: 10,
+//                                 ),
+//                                 SizedBox(
+//                                   width: 5,
+//                                 ),
+//                                 Text(
+//                                   'Unavailable now',
+//                                   style: TextStyle(
+//                                       fontSize: 13, color: Colors.red),
+//                                 ),
+//                               ],
+//                             )
+//                         ],
+//                       ),
+//                       const Row(
+//                         children: [
+//                           Text(
+//                             'Meeting room 204',
+//                             style: TextStyle(
+//                               fontSize: 13,
+//                             ),
+//                           ),
+//                           SizedBox(
+//                             width: 10,
+//                           ),
+//                           Icon(
+//                             Icons.circle,
+//                             color: Colors.black,
+//                             size: 10,
+//                           ),
+//                           SizedBox(
+//                             width: 10,
+//                           ),
+//                           Text('Floor 2')
+//                         ],
+//                       ),
+//                       Row(
+//                         children: [
+//                           Expanded(
+//                             child: Text(
+//                               widget.roomData.description ?? "",
+//                               style: const TextStyle(fontSize: 13),
+//                             ),
+//                           ),
+//                           const Icon(Icons.star_border_sharp),
+//                           Text(widget.roomData.rate.toString())
+//                         ],
+//                       )
+//                     ],
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
