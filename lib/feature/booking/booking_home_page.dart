604c0b2797eed89c584d4a8c8b848435c0b0f8f1
kwin <kwinthebest@gmail.com>
2024-03-01 10:09:49 +0700

    edit booking

--- lib/feature/booking/booking_home_page.dart
+++ lib/feature/booking/booking_home_page.dart
@@ -7,6 +7,8 @@ import 'package:qbooking/feature/booking/widget/slider_picture.dart';
 import 'package:qbooking/feature/booking/widget/text_picture_status.dart';
 import 'package:qbooking/feature/booking/widget/time_picker.dart';
 
+import '../../constant/colors_constant.dart';
+
 class BookingHomePage extends StatelessWidget {
   const BookingHomePage({super.key});
 
@@ -22,102 +24,112 @@ class BookingHomePage extends StatelessWidget {
             "Booking",
             style: TextStyle(color: Colors.black),
           ),
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(3),
+              child: Container(
+                color: ColorsConstants.borderAppbarColor,
+                height: 1,
+              )),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const TextPictureStatus(),
-                const SliderPicture(),
-                const SizedBox(
-                  height: 20,
-                ),
-                const DatePickUp(),
-                const SizedBox(
-                  height: 20,
-                ),
-                const TimePicker(),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Text(
-                  "EQUIPMENT",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Column(
-                  children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
+        body: Container(
+          decoration: const BoxDecoration(
+              color: ColorsConstants.primaryBackgroundColor),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TextPictureStatus(),
+                  const SliderPicture(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const DatePickUp(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const TimePicker(),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text(
+                    "EQUIPMENT",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Column(
+                    children: [
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            Equipment(
+                              titleEq: 'Microphone',
+                            ),
+                            Equipment(
+                              titleEq: 'TV',
+                            ),
+                            Equipment(
+                              titleEq: 'Board',
+                            ),
+                            Equipment(
+                              titleEq: 'plasma display',
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
                         children: [
                           Equipment(
-                            titleEq: 'Microphone',
+                            titleEq: 'Wifi',
                           ),
                           Equipment(
-                            titleEq: 'TV',
-                          ),
-                          Equipment(
-                            titleEq: 'Board',
-                          ),
-                          Equipment(
-                            titleEq: 'plasma display',
+                            titleEq: 'Capacity 15',
                           ),
                         ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        Equipment(
-                          titleEq: 'Wifi',
-                        ),
-                        Equipment(
-                          titleEq: 'Capacity 15',
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "DETAILS:",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                    "This room is equipped with all the necessary equipment for lectures, meetings and negotiations."),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ButtonBooking(
-                      titleButton: 'Booking now',
-                      color: Colors.black,
-                      titleColor: Colors.white,
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const QrUserPage()));
-                      },
-                    )
-                  ],
-                )
-              ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "DETAILS:",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                      "This room is equipped with all the necessary equipment for lectures, meetings and negotiations."),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ButtonBooking(
+                        titleButton: 'Booking now',
+                        color: Colors.black,
+                        titleColor: Colors.white,
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const QrUserPage()));
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
