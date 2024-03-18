b10cdbf58db0b8db70241dae23e44b6426e77c9e
kwin <kwinthebest@gmail.com>
2024-03-18 12:28:20 +0700

    validation Equipment

--- lib/feature/booking/presentation/sate/booking_room_state.dart
+++ lib/feature/booking/presentation/sate/booking_room_state.dart
@@ -1,8 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BookingRoomState with ChangeNotifier {
+  final List _addEquipment = [];
 
+  void addEquipment(int index) {
+    _addEquipment.add(index);
+    notifyListeners();
+  }
+  void removeEquipment(int index) {
+    _addEquipment.removeAt(index);
+    notifyListeners();
+  }
+  
 
-
-
+  bool isEquipment(int index) {
+    if (index == 0) {
+      addEquipment(index);
+      return true;
+    } else {
+      removeEquipment(index);
+      return false;
+    }
+  }
 }
+ 
--- lib/feature/booking/presentation/widget/equipment.dart
+++ lib/feature/booking/presentation/widget/equipment.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 
+import '../sate/booking_room_state.dart';
+
 class Equipment extends StatelessWidget {
-  const Equipment({super.key, required this.titleEq});
+  const Equipment({super.key, required this.titleEq, this.onTap});
   final String titleEq;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 7),
       child: InkWell(
-          onTap: () {},
+          onTap: () => BookingRoomState(),
           child: Container(
             decoration: BoxDecoration(
-                color: Colors.white,
+                color: BookingRoomState().isEquipment(0)
+                    ? Colors.white
+                    : Colors.blueAccent,
                 border: Border.all(color: Colors.black),
                 borderRadius: BorderRadius.circular(20)),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(titleEq),
+              child: Text(
+                titleEq,
+                style: TextStyle(
+                    color: BookingRoomState().isEquipment(0)
+                        ? Colors.black
+                        : Colors.red),
+              ),
             ),
           )),
     );
